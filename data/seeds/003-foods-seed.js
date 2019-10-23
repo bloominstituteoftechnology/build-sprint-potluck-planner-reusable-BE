4d5c419ab994e7ab00e04e639454081c261e824e
noahfranco <noahfra@gmail.com>
2019-10-23 12:31:03 -0400

    updated seed data

--- data/seeds/003-foods-seed.js
+++ data/seeds/003-foods-seed.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex("foods").insert([
-        {id: 1, food_item: "Hot dogs", events_id: "1"},
-        {id: 2, food_item: 'Nachos', events_id: "2"},
-        {id: 3, food_item: 'Macaroni and Cheese', events_id: "3"},
+        {id: 1, food_item: "Hot dogs", events_id: "1", completed: true },
+        {id: 2, food_item: 'Nachos', events_id: "2", completed: false },
+        {id: 3, food_item: 'Macaroni and Cheese', events_id: "3", completed: true },
       ]);
     });
 };
